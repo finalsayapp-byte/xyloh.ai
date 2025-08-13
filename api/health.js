85c5c3d86b6b66e49e71f0460760938d319c80ff
finalsayapp-byte <finalsayapp@gmail.com>
2025-08-12 18:47:58 -0700

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,8 +1,7 @@
 export default async function handler(req, res) {
-  const response = {
+  res.status(200).json({
     ok: !!process.env.OPENAI_API_KEY,
-    kv: !!process.env.KV_REST_API_URL && !!process.env.KV_REST_API_TOKEN,
-    env: Object.keys(process.env)
-  };
-  res.status(200).json(response);
+    KV: !!(process.env.KV_REST_API_URL && process.env.KV_REST_API_TOKEN),
+    vercelEnv: process.env.VERCEL_ENV || 'unknown'
+  });
 }
